4a5c1a1bafd9484a1f33c9d742efb597504e782f
Vladimír Antoš <vladimirantos@seznam.cz>
2023-01-21 21:06:16 +0100

    jwt auth

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,6 +3,7 @@
 use DI\ContainerBuilder;
 use Slim\App;
 use Dotenv\Dotenv;
+use Tuupola\Middleware\JwtAuthentication;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -14,4 +15,24 @@ $containerBuilder->addDefinitions(__DIR__ . '/container.php');
 $container = $containerBuilder->build();
 
 // Create App instance
-return $container->get(App::class);
+$app = $container->get(App::class);
+$app->add(new JwtAuthentication([
+	"path" => "/api", /* or ["/api", "/admin"] */
+	"ignore" => ["/api/public/"],
+	"attribute" => "current_user",
+	"secret" => $_ENV['JWT_SECRET_TOKEN'],
+	"algorithm" => ["HS256"],
+	"logger" => $container->get(\Psr\Log\LoggerInterface::class),
+	"secure" => false, //remove in debug
+	"error" => function ($response, $arguments) {
+		$data["status"] = "error";
+		$data["message"] = $arguments["message"];
+		return $response
+			->withHeader("Content-Type", "application/json")
+			->getBody()->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+	},
+	"callback" => function ($options) use ($app) {
+		$app->jwt = $options["decoded"];
+	}
+]));
+return $app;
